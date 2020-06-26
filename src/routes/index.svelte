2837fa0dcee8db99497db339fd2fcab0d7f057f3
saydus <saydus07@gmail.com>
2020-06-26 00:25:48 -0500

    Trying jQuery for AJAX now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,7 +20,7 @@ let formatWords = (str) => {
 
 
 // TODO: finish to the working upload of input words as a string
-let checkWords = (input) => {
+let checkWords = () => {
     // async request 
     fetch('/check', {
 		method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -110,13 +110,10 @@ a.btn.btn-link:focus{
 		<textarea class="form-control" bind:value={input} aria-label="With textarea" placeholder="Enter your text here."></textarea>
 	</div> 
 	<div class="button-container text-center">
-		<button type="button" class="btn btn-outline-primary text-center check-button" on:click={checkWords(input)}>Check</button>
+		<button type="button" class="btn btn-outline-primary text-center check-button" on:click={checkWords}>Check</button>
 	</div>
 
 
-
-
-	
 	<!-- Show only if words are entered --> 
 	<div id="accordion">
 		<div class="card">
