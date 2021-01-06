ccb3da5ecb3cdc48da122ca23d6e40006322bb50
saydus <saydus07@gmail.com>
2021-01-06 11:13:03 -0600

    Formatted the code

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Spelling Checker 
+# Spelling Checker
 
-This spelling checker will allow you to quickly validate your custom text input by just inserting it in the browser page. The usage of Trie data structure on back-end allows for a fast word-lookup, which makes the process of both adding custom dictionaries and validating text in `O(n)` for words with `n` characters. 
+This spelling checker will allow you to quickly validate your custom text input by just inserting it in the browser page. The usage of Trie data structure on back-end allows for a fast word-lookup, which makes the process of both adding custom dictionaries and validating text in `O(n)` for words with `n` characters.
 
 The default dictionary comes with all English words in it. You can overwrite it to check some other language. The website will allow adding more words to the dictionary, allowing to crowd-source the language vocabulary.
 
 ## Getting started
+
 Remember to `npm install` packages before running. To run the application: first, `npm run tsc`, and then, `npm start`.
 
 ### Validating text
+
 ![Screenshot of the main page](pics/screenshot.png)
 
-
 ### Adding custom words
+
 ![Screenshot of the add words page](pics/screenshot2.png)
--- src/server.js
+++ src/server.js
@@ -1,116 +1,103 @@
-import sirv from 'sirv';
-import express from 'express';
-import compression from 'compression';
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
 import * as sapper from "@sapper/server";
 
-import Trie from './trie.js';
+import Trie from "./trie.js";
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
-const app = express(); 
-  
+const app = express();
+
 const dictionary = new Trie.Trie();
-const lineReader = require('line-reader');
-
-
+const lineReader = require("line-reader");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
- 
+app.use(bodyParser.json());
 
 let getLettersOnly = (word) => {
-	// Replace all non-numeric 
-	return word.replace(/[^a-zA-Z_]/g, '').toLowerCase();
-}
+  // Replace all non-numeric
+  return word.replace(/[^a-zA-Z_]/g, "").toLowerCase();
+};
 
 let splitWords = (words) => {
-	// Use regular expression to split by comma or whitespace
-	return words.split(/[. ,:]+/);
-}
+  // Use regular expression to split by comma or whitespace
+  return words.split(/[. ,:]+/);
+};
 
 let formatWords = (str) => {
-	return splitWords(str).map(getLettersOnly).filter(Boolean);
-}
+  return splitWords(str).map(getLettersOnly).filter(Boolean);
+};
 
-let addFileToTrie = (filename) =>{
-	lineReader.eachLine(filename, (line, last) => {
-		// console.log(line);
-		dictionary.insert(line.toString()); 
-		// last is a boolean to indicate last line / not used for now
-	});
-}
-
-
+let addFileToTrie = (filename) => {
+  lineReader.eachLine(filename, (line, last) => {
+    // console.log(line);
+    dictionary.insert(line.toString());
+    // last is a boolean to indicate last line / not used for now
+  });
+};
 
 // Add standard English language
-addFileToTrie('words_alpha.txt');
+addFileToTrie("words_alpha.txt");
 
+app.post("/add", (req, res) => {
+  // add words from json to the Trie structure
+  console.log(req.body.words);
+  let words = req.body.words;
+  words.forEach((word) => {
+    dictionary.insert(word);
+  });
 
-app.post('/add',  (req, res) => {
-	// add words from json to the Trie structure
-	console.log(req.body.words);
-	let words = req.body.words;
-	words.forEach((word) => {
-		dictionary.insert(word);
-	})
-
-	// send response
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({ success: true}));
+  // send response
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ success: true }));
 });
 
+app.post("/addfast", (req, res) => {
+  // add words from json to the Trie structure
+  console.log(req.body.words);
+  let words = req.body.words.split(",");
+  words.forEach((word) => {
+    dictionary.insert(word);
+  });
 
-app.post('/addfast',  (req, res) => {
-	// add words from json to the Trie structure
-	console.log(req.body.words);
-	let words = req.body.words.split(',');
-	words.forEach((word) => {
-		dictionary.insert(word);
-	})
-
-	// redirect 
-	res.redirect('/');
+  // redirect
+  res.redirect("/");
 });
 
+app.post("/check", (req, res) => {
+  let userWords = req.body.words;
 
-app.post('/check', (req, res) => {
-	let userWords = req.body.words; 
+  userWords = formatWords(userWords.toString());
 
-	userWords = formatWords(userWords.toString());
+  let isCorrect = true;
+  let incorrectWords = [];
 
-	let isCorrect = true;
-	let incorrectWords = [];
-	
-	userWords.forEach((word)=>{
-		if(!dictionary.search(word)){ // if word is not in dictionary
-			incorrectWords.push(word); //push word to incorrect words
-			isCorrect = false;
-		}
-	});
+  userWords.forEach((word) => {
+    if (!dictionary.search(word)) {
+      // if word is not in dictionary
+      incorrectWords.push(word); //push word to incorrect words
+      isCorrect = false;
+    }
+  });
 
-	// send response
-	res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ allCorrect: isCorrect, words: incorrectWords}));
+  // send response
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ allCorrect: isCorrect, words: incorrectWords }));
 });
-
-
 
 app.use(
-	compression({ threshold: 0 }),
-	sirv('static', { dev }),
-	sapper.middleware()
+  compression({ threshold: 0 }),
+  sirv("static", { dev }),
+  sapper.middleware()
 );
 
-
 app.listen(PORT, function () {
-	console.log(`App is listening on port + ${PORT}`);
+  console.log(`App is listening on port + ${PORT}`);
 });
-
- 
